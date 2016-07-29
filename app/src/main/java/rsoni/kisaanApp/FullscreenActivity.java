aef1c78e19186806f7e216d91c94bc1653a308cf
DS1 <DS1@P409-imac.local>
2016-07-30 03:12:17 +0530

    test commit

---
+++ app/src/main/java/rsoni/kisaanApp/FullscreenActivity.java
@@ -0,0 +1,38 @@
+package rsoni.kisaanApp;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class FullscreenActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(FullscreenActivity.this,LoginActivity.class);
+                FullscreenActivity.this.startActivity(mainIntent);
+                FullscreenActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
+    }
+
+}
