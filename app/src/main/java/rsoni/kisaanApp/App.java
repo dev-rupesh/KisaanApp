6019fb0fc01896844268a174415a2899578b3886
DS1 <DS1@P409-imac.local>
2016-08-03 01:05:41 +0530

    test commit

---
+++ app/src/main/java/rsoni/kisaanApp/App.java
@@ -0,0 +1,18 @@
+package rsoni.kisaanApp;
+
+import android.app.Application;
+
+/**
+ * Created by DS1 on 03/08/16.
+ */
+public class App extends Application{
+
+    public static boolean is_loged = false;
+    public static
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
